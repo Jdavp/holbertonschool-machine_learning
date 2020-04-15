5cba48808f9f33461561bc21ee7280ba4f39d56d
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-04-15 17:40:58 +0000

    [new] add numer 2 file

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+    "returns the total size of all the list in a matrix"
+    total = []
+    item = matrix
+    while type(item) is list:
+        total.append(len(item))
+        item = item[0]
+    return total
