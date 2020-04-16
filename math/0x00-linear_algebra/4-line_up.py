e6e0a2e4dffcecaf3e23137a3fa62308e9d7eba5
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-04-16 22:53:40 +0000

    [new] first attempt 5

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"line up"
+def add_arrays(arr1, arr2):
+
+
+    "that adds two arrays element-wise"
+
+    if len(arr1) != len(arr2):
+        return None
+    else:
+        print(list(zip(arr1, arr2)))
+        return[x + y for x,y in zip(arr1, arr2)] 
---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"Across The Planes"
+
+
+def matrix_shape(matrix):
+    "returns the total size of all the list in a matrix"
+    total = []
+    item = matrix
+    while type(item) is list:
+        total.append(len(item))
+        item = item[0]
+    return total
+
+
+def add_matrices2D(mat1, mat2):
+    "adds two matrices element-wise"
+
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
+    else:
+        return [[mat1[x][y] + mat2[x][y] for y in range(len(mat1[0]))]
+                for x in range(len(mat1))]
