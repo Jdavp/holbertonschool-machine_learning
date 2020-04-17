dcd4083401c09a18c1031c90547a6b983fc5dc09
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-04-17 03:20:08 +0000

    [fix] fix condition

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python3
-"Across The Planes"
-
-
-def matrix_shape(matrix):
-    "returns the total size of all the list in a matrix"
-    total = []
-    item = matrix
-    while type(item) is list:
-        total.append(len(item))
-        item = item[0]
-        return total
+"Across The Planet"
 
 
 def add_matrices2D(mat1, mat2):
     "adds two matrices element-wise"
-    if matrix_shape(mat1) != matrix_shape(mat2[0]):
+    if len(mat1) != len(mat2[0]):
         return None
     else:
         return [[mat1[x][y] + mat2[x][y] for y in range(len(mat1[0]))]
