3e434e593c4dd7c2d0f06e1e5e22aaffce97bc9a
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-04-17 02:41:05 +0000

    [fix] add condition to when its both matrix are not the same shape

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,8 +4,5 @@
 
 def add_matrices2D(mat1, mat2):
     "adds two matrices element-wise"
-    if len(mat1) != len(mat2[0]):
-        return None
-    else:
-        return [[mat1[x][y] + mat2[x][y] for y in range(len(mat1[0]))]
-                for x in range(len(mat1))]
+    return [[mat1[x][y] + mat2[x][y] for y in range(len(mat1[0]))]
+            for x in range(len(mat1))]
