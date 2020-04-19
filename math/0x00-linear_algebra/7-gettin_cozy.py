cf6a61562ea9842dbee77114193157cff5ac98df
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-04-19 02:54:20 +0000

    [fix] make deep copy manual

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 "Gettin Cozy"
-import copy
+
+
+def cheat_copy(nested_content):
+    return eval(repr(nested_content))
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
     "concatenates two matrices along a specific axis"
-    copy_mat1 = copy.deepcopy(mat1)
-    copy_mat2 = copy.deepcopy(mat2)
+    copy_mat1 = cheat_copy(mat1)
+    copy_mat2 = cheat_copy(mat2)
     if (axis == 1):
         return [x + y for x, y in zip(copy_mat1, copy_mat2)]
     else:
