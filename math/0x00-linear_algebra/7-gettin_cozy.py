fd7f21b8ab4bba8764822722b8eea5a5defd2c63
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-04-19 05:33:18 +0000

    [fix] chamge logic for deep copy

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -2,14 +2,18 @@
 "Gettin Cozy"
 
 
-def cheat_copy(nested_content):
-    return eval(repr(nested_content))
+def deepcopy(obj):
+    if isinstance(obj, dict):
+        return {deepcopy(key): deepcopy(value) for key, value in obj.items()}
+    if hasattr(obj, '__iter__'):
+        return type(obj)(deepcopy(item) for item in obj)
+    return obj
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
     "concatenates two matrices along a specific axis"
-    copy_mat1 = cheat_copy(mat1)
-    copy_mat2 = cheat_copy(mat2)
+    copy_mat1 = deepcopy(mat1)
+    copy_mat2 = deepcopy(mat2)
     if (axis == 1):
         return [x + y for x, y in zip(copy_mat1, copy_mat2)]
     else:
