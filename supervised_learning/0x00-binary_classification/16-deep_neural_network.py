dbead2b7c96edac01d5b6381b731b4b9fc35f13c
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-11 05:24:16 +0000

    [fix]conditions

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -13,22 +13,22 @@ class DeepNeuralNetwork:
             raise TypeError('nx must be an integer')
         if nx <= 0:
             raise ValueError('nx must be a positive integer')
-
+        self.nx = nx
         if type(layers) is not list or len(layers) == 0:
             raise TypeError('layers must be a list of positive integers')
 
         self.L = len(layers)
         self.cache = {}
-        weight = {}
+        weights = {}
         for l in range(len(layers)):
             if layers[l] < 1:
-                raise ValueError('layers must be a list of positive integers')
+                raise TypeError('layers must be a list of positive integers')
             w_key = 'W'+str(l + 1)
-            if layers == 0:
-                weight[w_key] = np.random.randn(layers[l], nx)*np.sqrt(2 / nx)
-            else:
-                weight[w_key] = np.random.randn(layers[l], layers[l-1]) *\
-                                np.sqrt(2 / layers[l-1])
             b_key = 'b'+str(l + 1)
-            weight[b_key] = np.zeros((layers[l], 1))
-        self.weights = weight
+            if layers == 0:
+                weights[w_key] = np.random.randn(layers[l], nx)*np.sqrt(2 / nx)
+            else:
+                weights[w_key] = np.random.randn(layers[l], layers[l-1]) *\
+                                np.sqrt(2 / layers[l-1])
+            weights[b_key] = np.zeros((layers[l], 1))
+        self.weights = weights
