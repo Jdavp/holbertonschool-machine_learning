eb8a725971072b2199fb46a87cd2ef66d5547ba2
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-11 01:30:28 +0000

    [new]first attempt 8

---
+++ supervised_learning/0x00-binary_classification/8-neural_network.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'NeuralNetwork'
+import numpy as np
+
+
+class NeuralNetwork:
+    'neural network with one hidden layer performing binary classification'
+
+    def __init__(self, nx, nodes):
+        'class constructor'
+
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx <= 0:
+            raise ValueError('nx must be a positive integer')
+
+        if type(nodes) is not int:
+            raise TypeError('nodes must be an integer')
+        if nodes <= 0:
+            raise ValueError('nx must be a positive integer')
+
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        "output"
+        self.W2 = np.random.normal(size=(1, nodes))
+        self.b2 = 0
+        self.A2 = 0
