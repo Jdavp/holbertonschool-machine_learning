59a861abc278074bafdcac64eaddac555bc76fcf
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-12 20:29:57 +0000

    [fix]condition with la

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -7,27 +7,27 @@ class DeepNeuralNetwork:
     'deep neural network performing binary classification'
 
     def __init__(self, nx, layers):
-    'class constructor'
+        'class constructor'
 
-    if type(nx) is not int:
-        raise TypeError('nx must be an integer')
-    if nx < 1:
-        raise ValueError('nx must be a positive integer')
-    self.nx = nx
-    if type(layers) is not list or len(layers) == 0:
-        raise TypeError("layers must be a list of positive integers")
-    self.L = len(layers)
-    self.cache = {}
-    weights = {}
-    for l in range(len(layers)):
-        if layers[l] < 1:
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.nx = nx
+        if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        w_key = 'W'+str(l + 1)
-        b_key = 'b'+str(l + 1)
-        if layers == 0:
-            weights[w_key] = np.random.randn(layers[l], nx)*np.sqrt(2 / nx)
-        else:
-            weights[w_key] = np.random.randn(layers[l], layers[l-1]) *\
-                             np.sqrt(2 / layers[l-1])
-            weights[b_key] = np.zeros((layers[l], 1))
-    self.weights = weights
+        self.L = len(layers)
+        self.cache = {}
+        weights = {}
+        for la in range(len(layers)):
+            if layers[la] < 1:
+                raise TypeError("layers must be a list of positive integers")
+            w_key = 'W'+str(la + 1)
+            b_key = 'b'+str(la + 1)
+            if la == 0:
+                weights[w_key] = np.random.randn(layers[la], nx)*np.sqrt(2/nx)
+            else:
+                weights[w_key] = np.random.randn(layers[la], layers[la-1]) *\
+                             np.sqrt(2 / layers[la-1])
+                weights[b_key] = np.zeros((layers[la], 1))
+        self.weights = weights
