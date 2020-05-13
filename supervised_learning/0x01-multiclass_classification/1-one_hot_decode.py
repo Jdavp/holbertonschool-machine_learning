ca16c02cab14c9fdda4ec0c1ac6bf89c254dba55
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-13 04:34:02 +0000

    [add] add len condition to return none

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -6,6 +6,10 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     'converts a one-hot matrix into a vector of labels'
+    if len(one_hot) == 0 or len(one_hot.shape) != 2:
+        return None
+    if type(one_hot) is not np.ndarray:
+        return None
     try:
         return np.argmax(one_hot, axis=0)
     except Exception:
