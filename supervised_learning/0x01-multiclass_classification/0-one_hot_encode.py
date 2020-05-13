216ae06982a652879282c51b05a21c7a63ff9d6e
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-13 03:56:24 +0000

    [new] first attempt 0

---
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'One-Hot Encode'
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    'converts a numeric label vector into a one-hot matrix'
+
+    if type(classes) is not int or type(Y) is not np.ndarray:
+        return None
+    if classes is None or Y is None:
+        return None
+    encode = np.zeros((classes, len(Y)))
+    fila = np.arange(Y.shape[0])
+    encode[Y, fila] = 1
+    return encode
---
+++ supervised_learning/0x01-multiclass_classification/README.md
@@ -0,0 +1,9 @@
+Multiclass Classification
+
+General
+What is multiclass classification?
+What is a one-hot vector?
+How to encode/decode one-hot vectors
+What is the softmax function and when do you use it?
+What is cross-entropy loss?
+What is pickling in Python?
