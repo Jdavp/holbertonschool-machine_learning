f4789398974ee55dfffc9aa546c96d2e7b7ef5e6
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-14 05:42:56 +0000

    [new] first attemtp 22

---
+++ supervised_learning/0x00-binary_classification/22-deep_neural_network.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+'DeepNeuralNetwork'
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    'deep neural network performing binary classification'
+
+    def __init__(self, nx, layers):
+        'class constructor'
+
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        weights = {}
+        for la in range(len(layers)):
+            if layers[la] < 1:
+                raise TypeError("layers must be a list of positive integers")
+            w_key = 'W'+str(la + 1)
+            b_key = 'b'+str(la + 1)
+            if la == 0:
+                weights[w_key] = np.random.randn(layers[la], nx)*np.sqrt(2/nx)
+            else:
+                weights[w_key] = np.random.randn(layers[la], layers[la-1]) *\
+                                 np.sqrt(2 / layers[la-1])
+            weights[b_key] = np.zeros((layers[la], 1))
+        self.__weights = weights
+
+    @property
+    def weights(self):
+        'A dictionary to hold all weights and biased of the network'
+        return self.__weights
+
+    @property
+    def cache(self):
+        'A dictionary to hold all intermediary values of the network'
+        return self.__cache
+
+    @property
+    def L(self):
+        'The number of layers in the neural network'
+        return self.__L
+
+    def sigmoid(self, z):
+        "Apply sigmoid activation function"
+        return 1/(1+np.exp(-z))
+
+    def forward_prop(self, X):
+        'Calculates the forward propagation of the neural network'
+        self.__cache['A0'] = X
+
+        for layer in range(self.__L):
+            z = np.matmul(self.__weights['W'+str(layer+1)],
+                          self.__cache['A'+str(layer)])\
+                          + self.__weights['b'+str(layer+1)]
+            actived = self.sigmoid(z)
+            self.__cache['A'+str(layer+1)] = actived
+
+        return actived, self.__cache
+
+    def cost(self, Y, A):
+        "cost of the model using logistic regression"
+        lost = (np.multiply(np.log(A), Y) +
+                np.multiply(np.log(1.0000001-A), (1-Y)))
+        m = Y.shape[1]
+        cost = -np.sum(lost)/m
+        return cost
+
+    def evaluate(self, X, Y):
+        'Evaluates the neural network’s predictions'
+        A, _ = np.array(self.forward_prop(X))
+        pred = np.where(A >= 0.5, 1, 0)
+        cost = self.cost(Y, A)
+        return pred, cost
+
+    def gradient_descent(self, Y, cache, alpha=0.05):
+        'Calculates one pass of gradient descent on the neural network'
+        m = Y.shape[1]
+        for layer in reversed(range(self.__L)):
+            A = cache['A' + str(layer + 1)]
+            A_dw = cache['A' + str(layer)]
+            if layer == self.__L - 1:
+                dz = A - Y
+                W = self.__weights['W' + str(layer + 1)]
+            else:
+                da = A * (1 - A)
+                dz = np.matmul(W.T, dz)
+                dz = dz * da
+            W = self.__weights['W' + str(layer + 1)]
+            dw = np.matmul(A_dw, dz.T) / m
+            db = np.sum(dz, axis=1, keepdims=True) / m
+            self.__weights['W' + str(layer + 1)] = (self.__weights
+                                                    ['W'+str(layer+1)]
+                                                    - alpha * dw.T)
+            self.__weights['b' + str(layer + 1)] = (self.__weights
+                                                    ['b'+str(layer+1)]
+                                                    - alpha * db)
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        'trains neuron'
+        if type(iterations) is not int:
+            raise TypeError('iterations must be an integer')
+        if iterations <= 0:
+            raise ValueError('iterations must be a positive integer')
+        if type(alpha) is not float:
+            raise TypeError('alpha must be a float')
+        if alpha <= 0:
+            raise ValueError('alpha must be positive')
+        for it in range(iterations):
+            _, cache = self.forward_prop(X)
+            self.gradient_descent(Y, cache, alpha)
+        return self.evaluate(X, Y)
