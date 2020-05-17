be7d89bb43029cbdbae38a8b6a625503c9d47007
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-17 22:03:59 +0000

    [fix]change to float 32

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -5,6 +5,6 @@ import tensorflow as tf
 
 def create_placeholders(nx, classes):
     'returns two placeholders, x and y, for the neural network'
-    x = tf.placeholder("float", shape=[None, nx], name='x')
-    y = tf.placeholder("float", [None, classes], name='y')
+    x = tf.placeholder("float32", shape=[None, nx], name='x')
+    y = tf.placeholder("float32", [None, classes], name='y')
     return x, y
