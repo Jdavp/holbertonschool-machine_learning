a93683262498d8c087a2ae556fb357780f4365ef
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-17 16:48:58 -0500

    [new] first attemtp 0

---
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+'Placeholders'
+
+
+def create_placeholders(nx, classes):
+    'returns two placeholders, x and y, for the neural network'
+    x = tf.placeholder("float", shape=[None, nx], name='x')
+    y = tf.placeholder("float", [None, classes], name='y')
+    return x, y
