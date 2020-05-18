731cdebc13c0748c25c5d24c1828c0b00c7a35ed
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-18 04:52:52 +0000

    [new] first attempt 6

---
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+'train function'
+import tensorflow as tf
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
+
+
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
+          iterations, save_path="/tmp/model.ckpt"):
+
+    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    y_pred = forward_prop(x, layer_sizes, activations)
+    loss = calculate_loss(y, y_pred)
+    accuracy = calculate_accuracy(y, y_pred)
+    train_op = create_train_op(loss, alpha)
+
+    init = tf.initialize_all_variables()
+    with tf.Session() as session:
+        session.run(init)
+        for iter in range(iterations):
+            feed_dict = {'inputs': X_train[iter], 'labels': X_valid[iter]}
+            session.run(train_op, feed_dict=feed_dict)
+            if iter % 100 == 0:
+                print("After"+str(iter)+"iterations:")
+                print("Training Cost: "+str(loss))
+                print("Training Accuracy: "+str(accuracy))
+                print("Validation Cost: "+str(loss))
+                print("Validation Accuracy: "+str(accuracy))
+    return saver.save(session, save_path)
