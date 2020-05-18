c22d66b4f7501746eb576a368f51012d2abe4df9
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-18 01:56:48 +0000

    [fix] using argmax

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     'calculates the accuracy of a prediction'
-    predict = tf.equal(y, y_pred)
+    predict = tf.equal(tf.argmax(y, axis=1), tf.argmax(y_pred, axis=1))
     num = tf.cast(predict, tf.float32)
     mean = tf.reduce_mean(num)
     return mean
