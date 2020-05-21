4e45c023a3497dfef7a3bc793946e0c6a671538d
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-21 04:59:23 +0000

    [fix] logic

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -12,21 +12,33 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
           iterations, save_path="/tmp/model.ckpt"):
 
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
     y_pred = forward_prop(x, layer_sizes, activations)
+    tf.add_to_collection('y_pred', y_pred)
     loss = calculate_loss(y, y_pred)
+    tf.add_to_collection('loss', loss)
     accuracy = calculate_accuracy(y, y_pred)
+    tf.add_to_collection('accuracy', accuracy)
     train_op = create_train_op(loss, alpha)
-
-    init = tf.initialize_all_variables()
+    tf.add_to_collection('train_op', train_op)
+    saver = tf.train.Saver()
+    init = tf.global_variables_initializer()
     with tf.Session() as session:
         session.run(init)
-        for iter in range(iterations):
-            feed_dict = {'inputs': X_train[iter], 'labels': X_valid[iter]}
-            session.run(train_op, feed_dict=feed_dict)
-            if iter % 100 == 0:
-                print("After"+str(iter)+"iterations:")
-                print("Training Cost: "+str(loss))
-                print("Training Accuracy: "+str(accuracy))
-                print("Validation Cost: "+str(loss))
-                print("Validation Accuracy: "+str(accuracy))
-    return saver.save(session, save_path)
+        for iter in range(iterations+1):
+            train_dict = {x: X_train, y: Y_train}
+            valid_dict = {x: X_valid, y: Y_valid}
+            train_acc, train_los = session.run([accuracy, loss],
+                                               feed_dict=train_dict)
+            valid_acc, valid_los = session.run([accuracy, loss],
+                                               feed_dict=valid_dict)
+            if iter % 100 == 0 or iter == iterations:
+                print("After "+str(iter)+" iterations:")
+                print("Training Cost: "+str(train_los))
+                print("Training Accuracy: "+str(train_acc))
+                print("Validation Cost: "+str(valid_los))
+                print("Validation Accuracy: "+str(valid_acc))
+            if iter < iterations:
+                session.run(train_op, feed_dict=train_dict)
+        return saver.save(session, save_path)
