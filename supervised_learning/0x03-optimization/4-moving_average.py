e2f45ff221ac4c331af6c76ec1de0c347b10d5a4
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-24 23:54:21 +0000

    [new]first attempt 4

---
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'Moving Average'
+import tensorflow as tf
+
+
+def moving_average(data, beta):
+    'calculates the weighted moving average of a data set'
+    vt = 0
+    average = []
+    for v in range(len(data)):
+        vt = beta * vt + (1-beta) * data[v]
+        bias = 1 - beta ** (v+1)
+        average.append(vt/bias)
+    return average
