98596255e5231e9a63957975de49dfc4999e08c9
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-27 02:17:32 +0000

    [new] first attempt

---
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'F1 score'
+import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
+
+def f1_score(confusion):
+    'calculates the F1 score of a confusion matrix'
+    p = precision(confusion)
+    s = sensitivity(confusion)
+    return 2*p*s/(p+s)
