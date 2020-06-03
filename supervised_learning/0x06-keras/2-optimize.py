f9c22ab1edccc9167f7d794ff0a1429ee189cf04
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-06-03 04:42:45 +0000

    [new] first attempt 2

---
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'Optimize'
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    'Adam with categorical crossentropy loss and accuracy metrics'
+    adam = tf.keras.optimizers.Adam(
+        beta_1=beta1,
+        beta_2=beta2,
+        decay=alpha
+    )
+    return model.compile(loss="binary_crossentropy", optimizer=adam)
