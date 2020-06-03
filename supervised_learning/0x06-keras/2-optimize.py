5ba2d0770214cff5c5e66146cdc7c69517d3d37f
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-06-03 04:52:22 +0000

    [fix] change categorical crossenthropy

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -6,10 +6,10 @@ import tensorflow.keras as K
 def optimize_model(network, alpha, beta1, beta2):
     'Adam with categorical crossentropy loss and accuracy metrics'
     adam = K.optimizers.Adam(
+        alpha,
         beta_1=beta1,
         beta_2=beta2,
-        decay=alpha
     )
-    network.compile(loss="binary_crossentropy", optimizer=adam,
+    network.compile(loss="categorical_crossentropy", optimizer=adam,
                     metrics=["accuracy"])
     return None
