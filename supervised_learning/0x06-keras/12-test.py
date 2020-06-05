cf7104e347ee5f18ba0bee481d4ad2813cc02b67
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-06-05 03:37:33 +0000

    [new]assign verbose=verbose

--- supervised_learning/0x06-keras/12-test.py
+++ supervised_learning/0x06-keras/12-test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-'Test'
+'Test method'
 import tensorflow.keras as K
 
 
 def test_model(network, data, labels, verbose=True):
     'tests a neural network'
-    return network.evaluate(data, labels, verbose)
+    return network.evaluate(data, labels, verbose=verbose)
