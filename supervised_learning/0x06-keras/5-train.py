985e395225777918ff9b7e77b5aacca63de3d121
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-06-05 00:34:24 +0000

    [new]first attempt 5

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'Train'
+import tensorflow.keras as K
+
+
+def train_model(network, data,
+                labels, batch_size,
+                epochs, validation_data=None,
+                verbose=True, shuffle=False):
+    'also analyze validation data'
+    if validation_data is not None:
+        return network.fit(data, labels, epochs=epochs,
+                           batch_size=batch_size, verbose=verbose,
+                           shuffle=shuffle, validation_data=validation_data)
